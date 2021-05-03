8c4a9e26afc000359dc59fa35f006b03027de472
Julien Devot <jde@snaike.com>
2021-05-03 14:29:56 +0200

    first commit

---
+++ src/Controller/APIController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticlesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+/**
+ * @Route("/api", name="api_")
+ */
+class APIController extends AbstractController
+{
+    /**
+     * @Route("/articles/liste", name="liste", methods={"GET"})
+     */
+    public function liste(ArticlesRepository $articlesRepo)
+    {
+        return $this->json($articlesRepo->findall(),200,[], ['groups'=>'article:read']);
+    }
+    /**
+     * @Route("/article/liste/{id}", name="article", methods={"GET"})
+     */
+    public function getArticle(int $id,ArticlesRepository $articleRepo,SerializerInterface $serializer)
+    {
+        $article = $articleRepo->find($id);
+        $data = $serializer->serialize($article,'json');
+        return new Response($data,200,['Content-Type' => 'application/json']); 
+    }
+}
